5687314b3ecd88d70aab8dcd45dc3b512c82bb5e
Oksana Komarnytska <okazimiriv@gmail.com>
2023-04-11 23:13:39 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');  
+const stopBtn = document.querySelector('[data-stop]');  
+let timerId = null;
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {  
+  timerId = setInterval(() => {
+    const generetedColor = getRandomHexColor();
+    document.body.style.backgroundColor = generetedColor;
+    console.log(generetedColor);
+  }, 1000);  
+}
+
+function onStopBtn() {
+   clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+
+
 
