83c43f04b90d3bef7f1ea7b8b6a67fa79610fc7e
Okazimiriv <118137941+Okazimiriv@users.noreply.github.com>
2023-04-14 09:19:20 +0000

    Deploying to gh-pages from @ Okazimiriv/goit-js-hw-09@ba9b7adf5c6b332dbed64652f483ae3c01d93b12 🚀

--- 01-color-switcher.99a222a5.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;t.addEventListener("click",(function(){o=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t,console.log(t)}),1e3)})),e.addEventListener("click",(function(){clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.99a222a5.js.map
