5d291cd49f37bdf87fa21ac925e398cb1d558447
Oksana Komarnytska <okazimiriv@gmail.com>
2023-04-16 17:51:06 +0200

    task-3 finish fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,12 +13,12 @@ refs.form.addEventListener('click', onSubmitBtn);
 function onSubmitBtn(e) { 
   e.preventDefault();
   
-  let delay = Number(refs.inputDelay.value);
+  let valueDelay = Number(refs.inputDelay.value);
   let step = Number(refs.inputStep.value);
   let amount = Number(refs.inputAmount.value);  
 
   for (let position = 1; position <= amount; position += 1) {
-     let delayPromise = delay + step * position;      
+     let delayPromise = valueDelay + step * position;      
   
     createPromise(position, delayPromise)
       .then(({ position, delay }) => {
@@ -26,15 +26,16 @@ function onSubmitBtn(e) {
     })
       .catch(({ position, delay }) => {      
       Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    });     
+      });
+    refs.form.reset();
   } 
-   refs.form.reset();
+   
 }
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {    
     const shouldResolve = Math.random() > 0.3;
-    
+
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
