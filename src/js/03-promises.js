fb3603f390d59ac9b04238e25a3792130c871d87
Oksana Komarnytska <okazimiriv@gmail.com>
2023-04-16 17:42:04 +0200

    8

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,34 +15,34 @@ function onSubmitBtn(e) {
   
   let delay = Number(refs.inputDelay.value);
   let step = Number(refs.inputStep.value);
-  let amount = Number(refs.inputAmount.value); 
+  let amount = Number(refs.inputAmount.value);  
 
   for (let position = 1; position <= amount; position += 1) {
      let delayPromise = delay + step * position;      
-
+  
     createPromise(position, delayPromise)
-    .then(({ position, delay }) => {
+      .then(({ position, delay }) => {
       Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
     })
-    .catch(({ position, delay }) => {
+      .catch(({ position, delay }) => {      
       Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
-     
+    });     
   } 
    refs.form.reset();
 }
 
 function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {    
     const shouldResolve = Math.random() > 0.3;
     
-    setTimeout(() => {      
+    setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
         reject({ position, delay });
       }
-    }, delay)
+    }, delay);
   });
 }
+
 
